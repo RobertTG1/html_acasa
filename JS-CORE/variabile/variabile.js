aa50fe2a33584891c7a26198499b6f4338f9a95d
robert <robert.moraru.luca@gmail.com>
2022-01-18 20:43:37 +0200

    javasc

---
+++ JS-CORE/variabile/variabile.js
@@ -0,0 +1,10 @@
+// punem data curenta in variabila date
+var date  = new Date()
+
+var hour = date.getHours()
+var isRaining = false
+var message = "Hello! The clock is " + hour
+// o variabila fara valoare (undefined)
+var city
+
+alert(message)
