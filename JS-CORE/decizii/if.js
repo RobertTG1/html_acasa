6e6f8c6e0511298ddb6bdfeb99b5a290dd67a1fd
RobertTG1 <robert.moraru.luca@gmail.com>
2022-01-20 18:39:37 +0200

    java

---
+++ JS-CORE/decizii/if.js
@@ -0,0 +1,23 @@
+
+var isRaining = false
+var isSunny = false
+var isSnowing = false
+
+if (isRaining) {
+    document.write('Ia-ti o umbrela!')
+}else if(isSunny){
+    document.write('Nu uita crema de soare!')
+}
+else if(isSnowing){
+    document.write('Facem un om de zapada')
+}
+else{
+    document.write('Stam acasa!')
+}
+/* Conditia if else
+if (condition) {
+    
+} else {
+    
+}
+*/
--- JS-CORE/variabile/variabile.js
+++ JS-CORE/variabile/variabile.js
@@ -1,11 +1,17 @@
 // punem data curenta in variabila date
 var date  = new Date()
 
+
 var hour = date.getHours()
 var minutes = date.getMinutes()
+var seconds = date.getSeconds()
 var isRaining = false
-var message = "Hello! The clock is " + hour + ":" + minutes
+//am adaugat la un sir o variabila(concatare)
+var message = "Hello! The clock is " + hour + ":" + minutes + ":" + seconds
 // o variabila fara valoare (undefined)
 var city
 
-alert(message)
+// alert(message)
+console.log(message)
+
+document.write('<h1>' + message + '</h1>') 
