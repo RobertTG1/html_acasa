eab9fc47e5d4cff02786a22c1220d41d0ea62c0b
RobertTG1 <robert.moraru.luca@gmail.com>
2022-01-25 18:47:43 +0200

    js

---
+++ JS-CORE/bucle/for.js
@@ -0,0 +1,10 @@
+var i = 0
+while(i < 5) {
+    console.log(i)
+    i++
+}
+
+
+for (let i = 0; i < 5; i++) {
+   console.log(i)
+}
--- JS-CORE/bucle/inmultire.js
+++ JS-CORE/bucle/inmultire.js
@@ -1,5 +1,5 @@
 
-var i = 2;
+var i = 3;
 var j = 1;
 
 var max = 10;
@@ -8,7 +8,18 @@ while (j <= max) {
     //string cu concatenare:
     var message = (j + "x" + i + "=" + i * j + "<br>")
     //string cu interpolare:
-    message = `${j} x ${i} = ${j*i} <br>`
+    message = `${j} x ${i} = ${j * i} <br>`
     document.write(message)
     j++
+}
+
+document.write('<br>')
+
+for (i = 1;i <=max;i++) {
+
+
+    for (j = 1; j <= max; j++) {
+        var message = `${j} x ${i} = ${j * i} <br>`
+        document.write(message)
+    }
 }
