7c044f9e4c494b8df9875f38249e5fcdc827ba3e
robert <robert.moraru.luca@gmail.com>
2022-02-03 20:47:13 +0200

    js

--- JS-CORE/speed/speed.js
+++ JS-CORE/speed/speed.js
@@ -5,13 +5,38 @@ function randomNumber(max) {
 
 function randomLetter() {
     let letters = 'ABCDEFGHIJKLMNOPQRSTUVWYZ'
-    let i = randomNumber(letters.length)
+    let i = randomNumber(letters.length - 1)
     return letters[i]
 }
 
-function randomNumber() {
+function randomColor() {
     let red = randomNumber(255)
     let green = randomNumber(255)
     let blue = randomNumber(255)
     return `rgb(${red},${green},${blue})`
 }
+
+function randomLetterElement() {
+    let div = document.createElement('div')
+    div.innerText = randomLetter()
+    div.style.backgroundColor = randomColor()
+    div.style.top = randomNumber(99) + 'vh'
+    div.style.left = randomNumber(99) + 'vw'
+    div.classList.add('letter')
+    document.querySelector('body').append(div)
+
+}
+
+async function run() {
+    var gameOver = false
+    while (!gameOver) {
+        randomLetterElement()
+        await new Promise(run => setTimeout(run, 1))
+    }
+}
+
+function hideStart() {
+    document.querySelector('input').style.display = 'none'
+}
+
+document.querySelector('input').addEventListener('click', run)
